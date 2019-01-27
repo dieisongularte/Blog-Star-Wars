89b51aad4eaf1104cee9d088e3f5a8e2f4d25965
Dieison Gularte <dieissongularte@gmail.com>
2019-01-27 18:46:09 -0200

    Update navbar and route default

--- BlogStarWars/Startup.cs
+++ BlogStarWars/Startup.cs
@@ -56,7 +56,7 @@ namespace BlogStarWars
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Posts}/{action=Index}/{id?}");
             });
         }
     }
