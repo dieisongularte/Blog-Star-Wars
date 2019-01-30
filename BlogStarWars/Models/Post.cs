9797602c279ee26d466a421a38a239d817e671f0
Dieison Gularte <dieissongularte@gmail.com>
2019-01-30 16:48:02 -0200

    Implements view create e validation

--- BlogStarWars/Models/Post.cs
+++ BlogStarWars/Models/Post.cs
@@ -1,12 +1,40 @@
-﻿namespace BlogStarWars.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogStarWars.Models
 {
     public class Post
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [MinLength(3)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [MinLength(3)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [MinLength(3)]
         public string Content { get; set; }
+
+        [Display(Name = "Likes")]
         public int QuantLikes { get; set; }
+
+        [Display(Name = "Views")]
         public int QuantViews { get; set; }
+
+        public Post()
+        {
+        }
+
+        public Post(string title, string descripiton, string content)
+        {
+            Title = title;
+            Description = descripiton;
+            Content = content;
+            QuantLikes = 0;
+            QuantViews = 0;
+        }
     }
 }
